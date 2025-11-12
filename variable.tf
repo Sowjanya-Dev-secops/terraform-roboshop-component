1d5b406f3811fe0c8d63394cd18e795b0256bd0f
sowjanya <sowji.muppala@gmail.com>
2025-11-12 17:30:51 +0530

    practice

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09288351F1RG63CMSZ8A"
+  
+}
+variable "domain_name" {
+  default = "msdevsecops.fun"
+}
+variable "component" {
+  
+}
+variable "rule_priority" {
+  
+}
